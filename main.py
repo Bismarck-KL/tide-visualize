147751d4d68e30ae6ac072b9d22520a955fa1e3b
Bismarck Ng <bismarck4game@gmail.com>
2025-09-15 16:42:38 +0800

    feat(main): create function to get the data in html and print out the data

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-print("hello world")
+import requests
+from lxml import html
+import numpy as np
+import dotenv
+import os
+
+# Load environment variables
+dotenv.load_dotenv()
+
+# Get the year and filename from the environment variables
+year = int(os.getenv('YEAR', 2024))
+filename = os.getenv('FILENAME', f"page-{year}.html")
+
+# Fetch data
+if not os.path.exists(filename):
+    page = requests.get(os.getenv('URL')).text
+    with open(filename, 'w', encoding='UTF8') as f:
+        f.write(page)
+else:
+    with open(filename, 'r', encoding='UTF8') as f:
+        page = f.read()
+
+# Parse the page to HTML
+tree = html.fromstring(page)
+rows = tree.xpath(os.getenv('ROW_XPATH'))
+
+data_list = []
+for row in rows: 
+    parts = row.text_content().split()
+    if parts[0] != "Date" and len(parts) == 25:
+        data_list.append([float(x) for x in parts])
+
+# Convert the data_list to a 2D NumPy array
+data_array = np.array(data_list)
+print("data_array:",data_array)
+
